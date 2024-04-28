7793fb17da171fa7a5668b416b471dd99eb6f6ce
Vitor Koch <vitorgabrielkoch@gmail.com>
2024-04-28 17:21:00 -0300

    style: change theme to catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
-  "sainnhe/gruvbox-material",
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox-material",
+      colorscheme = "catppuccin-mocha",
     },
   },
 }
