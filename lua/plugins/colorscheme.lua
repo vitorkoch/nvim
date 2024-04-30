8ef273f9b279d4fe57f61e572aef62a8fe8bea49
Vitor Koch <vitorgabrielkoch@gmail.com>
2024-04-30 19:07:13 -0300

    feat/style: add auto-save and gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "sainnhe/gruvbox-material", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "gruvbox-material",
     },
   },
 }
